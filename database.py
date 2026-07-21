9e94f80097503e269c964040cc1b88cf95c50cb1
Parmar Gautam k <gkparmar505@gmail.com>
2026-07-21 08:13:41 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://ruvira_db_user:d3OQlGX3QrD0fs5B7MfFtjuM9ivZYkar@dpg-d8pr18jsq97s738cqdvg-a/ruvira_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
